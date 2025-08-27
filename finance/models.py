cdd8833d3e8802dec326c7a02af624f3931cc10d
Dan Louis Dela Cruz <louisdc002@gmail.com>
2025-08-27 22:55:58 +0800

    Create budget, expense, and category models

--- finance/models.py
+++ finance/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Budget(models.Model):
+    STATUS_CHOICES = [
+        ('ACTIVE', 'Active'),
+        ('ARCHIVED', 'Archived'),
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    total_amount = models.FloatField()
+    start_date = models.DateField()
+    end_date = models.DateField(null=True)
+    status = models.CharField(max_length=100, choices=STATUS_CHOICES, default='ACTIVE')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Expense(models.Model):
+    budget = models.ForeignKey(Budget, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField()
+    amount = models.FloatField()
+    date = models.DateField()
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True, blank=True)
+    notes = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
